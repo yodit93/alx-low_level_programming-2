635c574c03f0459cd8d80618d17b88a4d8492335
yodit93 <yoditabebe9391@gmail.com>
2022-06-30 08:38:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  *main - entry point
  *
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19
-\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 
