a1388753b1aefe3f6c1b67247c721a279707bea9
yodit93 <yoditabebe9391@gmail.com>
2022-07-01 09:28:59 -0700

    second commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,32 +5,17 @@
 #include <stdio.h>
 
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success/correct)
-
  */
 
 int main(void)
-
-
-
 {
-
 int n;
-
-
-
 srand(time(0));
 
 n = rand() - RAND_MAX / 2;
-
-
-
-
 
 if (n % 10 > 5)
 
