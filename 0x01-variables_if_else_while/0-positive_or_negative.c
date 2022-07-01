edb06b072d2cf5b80c66bfaa34c0e668a4e00e96
yodit93 <yoditabebe9391@gmail.com>
2022-07-01 06:59:05 -0700

    posetive is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,30 @@
 #include <stdlib.h>
-
 #include <time.h>
-
-/* more headers goes there */
-*
-*
-*
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Aiways 0 (success/correct)
+ */
 int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
+n=rand() - RAND_MAX / 2;
+
+if (n==0)
 {
-printf("%i is positive\n", n);
+	printf("%i is zero\n", n);
 }
-else if (n == 0)
+else if (n<0)
 {
-printf("%i is zero\n", n);
+	printf("%i is negative\n", n);
 }
-else 
-{ 
-printf("%i is negative\n", n);
-}	  
+else
+{
+	printf("%i is posetive\n", n);
+}
 return (0);
-}
+
+
+
