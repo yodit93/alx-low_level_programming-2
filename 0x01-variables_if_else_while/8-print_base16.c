e48b334455bd6580e0b269214810c0c5a97dcb11
yodit93 <yoditabebe9391@gmail.com>
2022-07-02 05:01:51 -0700

    hexadecimal digits

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,11 @@ int main(void)
 {
 char hexa;
 
-for (hexa =48; hexa <= 57; hexa++)
+for (hexa = 48; hexa <= 57; hexa++)
 putchar(hexa);
 
-for (hexa =97; hexa <= 102; hexa++)                                                                                                                    putchar(hexa);
+for (hexa = 97; hexa <= 102; hexa++)
+putchar(hexa);
 
 putchar('\n');
 
