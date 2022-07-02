c07dea6548ed1efb2e6485920f6cc77333c48246
yodit93 <yoditabebe9391@gmail.com>
2022-07-02 01:48:42 -0700

    first digit numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+
+{
+	char num;
+
+	for (num = '0'; num < '10'; num++)
+		putchar(num);
+
+	putchar('\n');
+
+return (0);
+
+}
