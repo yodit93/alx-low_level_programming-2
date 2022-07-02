4cdf9c2e5f5306279dbf9d777b541366a0a018e0
yodit93 <yoditabebe9391@gmail.com>
2022-07-02 04:29:47 -0700

    hexadecimal digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char hexa;
+
+for (hexa = '0'; hexa <= 'f'; hexa++)
+putchar(hexa);
+
+putchar('\n');
+
+return (0);
+
+}
+
