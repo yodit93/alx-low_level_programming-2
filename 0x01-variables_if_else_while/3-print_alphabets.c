6328d310577595ace8c7220a188d78b71a27db12
yodit93 <yoditabebe9391@gmail.com>
2022-07-02 00:03:24 -0700

    Upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char upper_case;
+	char lower_case;
+	for (lower_case = 'a'; lower_case > 'z'; lower_case++){
+		putchar(lower_case);
+	}
+	
+	
+	for (upper_case = 'A'; upper_case > 'Z'; upper_case++){
+		putchar(upper_case);
+	}
+putchar('\n');
+return (0);
+}
+
