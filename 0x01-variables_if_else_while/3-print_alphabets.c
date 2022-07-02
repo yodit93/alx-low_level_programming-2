ff80c6f4f89c0916648aebd4f71d7de79bdfbfc8
yodit93 <yoditabebe9391@gmail.com>
2022-07-02 00:42:57 -0700

    lower case and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	char upper_case;
 	char lower_case;
-	for (lower_case = 'a'; lower_case > 'z'; lower_case++){
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++){
 		putchar(lower_case);
 	}
 	
 	
-	for (upper_case = 'A'; upper_case > 'Z'; upper_case++){
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++){
 		putchar(upper_case);
 	}
 putchar('\n');
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
+
+
