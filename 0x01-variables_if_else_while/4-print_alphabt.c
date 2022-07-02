21d35d75b8aa26c9c27990eefa7510884ce40b3b
yodit93 <yoditabebe9391@gmail.com>
2022-07-02 00:24:51 -0700

    lower case except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char lower_case;
+	for (lower_case = 'a'; lower_case > 'z'; lower_case++)
+	{
+		if (lower_case == 'e' || lower_case == 'q')
+		{
+			continue;
+		}
+
+		putchar(lower_case);
+	}
+	putchar('\n');
+return (0);
+}
+
