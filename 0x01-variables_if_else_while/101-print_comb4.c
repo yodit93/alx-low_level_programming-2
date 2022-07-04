b9f8467383ebe6311b33b3d5c65d7433056ffc67
yodit93 <yoditabebe9391@gmail.com>
2022-07-04 02:08:43 -0700

    possible two combinations of digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,34 +5,31 @@
  * Return: 0
  */
 int main(void)
+
 {
-int i;
+int i,j,k;
 
-int j;
-
-for (i = 48; i <= 57; i++)
+for (i = 48; i <=57; i++)
 {
 for (j = i + 1; j <= 57; j++)
 {
+for (k = j + 1; k <= 57; k++)
+{
 putchar(i);
 putchar(j);
-
-if ((i == 56) && (j == 57))
-{
+putchar(k);
+if ((i == 55) && (j == 56) && (k == 57))
 break;
-}
-
-
 putchar(',');
 putchar(' ');
 
 }
+
 }
 
+}
 putchar('\n');
 
 return (0);
-
 }
-
 
