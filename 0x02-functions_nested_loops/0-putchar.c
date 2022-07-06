1f86c35dc1363f5471f15b81f523f8493f4088af
yodit93 <yoditabebe9391@gmail.com>
2022-07-06 01:08:28 -0700

    main header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  *
@@ -8,8 +8,17 @@ int main(void)
 
 {
 
-_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 
 }
+
