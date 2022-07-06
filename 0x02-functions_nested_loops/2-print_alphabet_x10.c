9438732b07c8b4d6865cc05f2b888caf6f940d41
yodit93 <yoditabebe9391@gmail.com>
2022-07-06 02:01:26 -0700

    small letters 10 x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * REturn: 0
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+	char alphabet;
+	
+	for (i = 1; i <= 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+
+	_putchar('\n');
+
+	}
+}
+
