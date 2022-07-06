2193987ae1114ae60e8cc84f862b492370da6730
yodit93 <yoditabebe9391@gmail.com>
2022-07-06 06:16:33 -0700

    print the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - function for printing last digit
+ * @n: number whose abs will be calculated
+ * Return: abs of a number
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
