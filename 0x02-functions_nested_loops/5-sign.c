1cb87beba45d2a6c8bf54e02785d7fde919fecc5
yodit93 <yoditabebe9391@gmail.com>
2022-07-06 05:33:59 -0700

    print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - function for checking the sign of a given number
- * @n - a number checked for its sign
+ * @n: a number checked for its sign
  * Return: + if posetive, - if negative and 0 if neither or if it is 0
  */
 int print_sign(int n)
