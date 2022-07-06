8940a024332db446ed2dd701466277739518ffbc
yodit93 <yoditabebe9391@gmail.com>
2022-07-06 07:08:07 -0700

    print the time in 24 hour format

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main"
+/**
+ * jack_bauer - fiunction to print the time in 24 hour format
+ */
+void jack_bauer(void)
+{
+	int a;
+
+	int b;
+
+for (a = 0; a <= 23; a++)
+
+{
+
+for (b = 0; b <= 59; b++)
+
+{
+
+_putchar (a / 10 + '0');
+
+_putchar (a % 10 + '0');
+
+_putchar (':');
+
+_putchar (b / 10 + '0');
+
+_putchar (b % 10 + '0');
+
+_putchar ('\n');
+
+}
+
+}
+
+}
