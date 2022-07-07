bd4c19d7b4ce3f83b3a98001bd6bb46eab7ac54f
yodit93 <yoditabebe9391@gmail.com>
2022-07-07 08:27:15 -0700

    print num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,11 @@
 /**
  * print_numbers - prints numbers from 0 to 9
  * @i: numbers
- * Return: 0
  */
 void print_numbers(void)
 {
 int i;
+
 while (i < 10)
 {
 _putchar(i + '0');
@@ -15,5 +15,4 @@ i++;
 _putchar('\n');
 
 }
-
 
