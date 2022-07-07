021c5d74ef813b0826c57cf5af3d52b21385301b
yodit93 <yoditabebe9391@gmail.com>
2022-07-07 07:08:25 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_line - prints line
+ * @n: the number of times the character - will be printed
+ * Return: 0 Always
+ */
+void print_line(int n)
+{
+
+int i;
+
+
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+_putchar(95); 
+}
+
+_putchar('\n'); 
+}
+
+else
+{
+_putchar('\n');
+}
+
+}
+
