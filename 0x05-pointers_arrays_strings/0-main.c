cfbe00652687170f5426f115dc4bf4d3a3b367e4
yodit93 <yoditabebe9391@gmail.com>
2022-07-13 00:41:50 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int n;
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n = %d\n", n);
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the number to 98
+ * @*n: pointer
+ * Return: 0 always
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
