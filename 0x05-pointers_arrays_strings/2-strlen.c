bd646457095da4186b4e5f3fac68a98e18ec61e4
yodit93 <yoditabebe9391@gmail.com>
2022-07-13 02:22:41 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - counts the length of the string given
+ * @s: pointer that points to a given string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int u;
+
+	u = 0;
+
+	while (s[u] != \0)
+	{
+		u++;
+	}
+	return (u);
+}
+
