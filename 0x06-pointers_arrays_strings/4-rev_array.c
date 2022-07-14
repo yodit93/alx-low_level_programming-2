80fab0c55044a6eac89e2fea545abc37361698e3
yodit93 <yoditabebe9391@gmail.com>
2022-07-14 04:20:00 -0700

    reverse string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * reverse_array - reverses array elements
+ * @a: array
+ * @n: elements of array
+ * Return: 0
+ */
+void reverse_array
+{
+	int i;
+	int *start_c, *end_c, c;
+
+	start_c = a;
+	end_c = a;
+
+
+	for (i = 0; i < n; i++)
+	{
+		end_c++;
+	}
+
+	for (i = 0; i < n / 2; i++)
+	{
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+
+		start_c++;
+		end_c--;
+	}
+}
+
+	
+
+
