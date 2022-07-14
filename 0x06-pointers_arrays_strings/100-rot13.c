7227ce3cf1520135dd90e7bcf22922a6a6382229
yodit93 <yoditabebe9391@gmail.com>
2022-07-14 05:35:27 -0700

    encode to rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *rot13 - encodes to rot13
+ * @s: string
+ * Return: 0
+ */
+char *rot13(char *s)
+{
+int count = 0, i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+while (*(s + count) != '\0')
+{
+
+for (i = 0; i < 52; i++)
+{
+
+if (*(s + count) == alphabet[i])
+{
+*(s + count) = rot13[i];
+break;
+}
+}
+
+count++;
+}
+
+return (s);
+}
+
