5557ff70ec7570568112dcc5ddb8552cf77aa240
yodit93 <yoditabebe9391@gmail.com>
2022-07-14 02:55:27 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncpy - copies string
+ * @dest: poiter to the character to be copied
+ * @src: pointer to the string
+ * @n: value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
