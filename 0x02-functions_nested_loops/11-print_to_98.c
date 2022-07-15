82f432effa70abd06637615ca6ac898bdbb8ec4d
yodit93 <yoditabebe9391@gmail.com>
2022-07-15 07:44:22 -0700

    print num upto 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_to_98 - prints number starting from n
+ * @n: staring num
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+
+
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d, ", i);
+	}
+}
+
