4a9ab7a8d50c1154098b9258a0cc2f7a260cc667
yodit93 <yoditabebe9391@gmail.com>
2022-07-15 01:38:00 -0700

    modifying a[2] to 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - modify the value of the array second element
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -8,6 +12,7 @@ int a[5];
 int *p;
 
 a[2] = 1024;
+
 p = &n;
 
 /*
@@ -17,9 +22,14 @@ p = &n;
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
+
 p[5] = 98;
+
 /* ...so that this prints 98\n */
+
 printf("a[2] = %d\n", a[2]);
+
 return (0);
+
 }
 
