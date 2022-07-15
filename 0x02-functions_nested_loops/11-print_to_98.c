9fac6a711f327476bbeb443642ede1c07b7a5450
yodit93 <yoditabebe9391@gmail.com>
2022-07-15 07:51:02 -0700

    print num upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,20 @@ void print_to_98(int n)
 
 	int i;
 
+	if (n <= 98)
+	{
+
 	for (i = n; i <= 98; i++)
 	{
 		printf("%d, ", i);
+	}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
 	}
 }
 
