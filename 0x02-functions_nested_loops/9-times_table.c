d4ec093cfd764549273d66919af4b6616cd4a65c
yodit93 <yoditabebe9391@gmail.com>
2022-07-15 06:00:28 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,6 +22,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
@@ -30,7 +31,6 @@ void times_table(void)
 				if (j == 9)
 					continue;
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
 		}
