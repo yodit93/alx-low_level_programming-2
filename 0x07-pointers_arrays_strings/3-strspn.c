02a11778ed0045dbee294dcc80c2c5c01c584099
yodit93 <yoditabebe9391@gmail.com>
2022-07-19 02:51:30 -0700

    compares the similarity of two strings and return the length of similarirty

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn- compares two string and returns the length
+ * @s: the first string
+ * @accept: the second string
+ * Return: the length og similar characters
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, length;
+
+	length = 0;
+
+	for (i = 0; s[i] != ','; a++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				length++;
+		}
+	}
+	return (length);
+}
+
