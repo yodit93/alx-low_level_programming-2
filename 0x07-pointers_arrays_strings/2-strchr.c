27504fa6ad8f85d67d6745eae097c8353e14a888
yodit93 <yoditabebe9391@gmail.com>
2022-07-19 01:07:17 -0700

    check the occurence of a character c in the string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strchr - find a character from the string given
+ * @s: string
+ * @c: character
+ * Return: s or null base don condition
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int a;
+
+	for (a = 0; s[i]; a++)
+	{
+		if (s[i] == c)
+			return (s);
+		return ('\0');
+	}
+}
+
+
+
