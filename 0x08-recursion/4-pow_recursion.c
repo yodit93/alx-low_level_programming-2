242681f1ef53acc975fbde5e7666c5be9a55b5d0
yodit93 <yoditabebe9391@gmail.com>
2022-07-20 05:25:20 -0700

    power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - return num power to a num
+ * @x: the number to be powered
+ * @y: the power
+ * Return: power of a number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(y - 1));
+}
+
