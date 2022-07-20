a7f2091ea02732184e1abc41fea066da7b83cd52
yodit93 <yoditabebe9391@gmail.com>
2022-07-20 09:12:37 -0700

    palindrome string

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - size
+ * @c: pointer to string params
+ * Return: recursion
+ */
+int _strlen_recursion(char *c)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+int p1(char *s, int l)
+	if (l < 1)
+		return (1);
+	if (*s == *(s + l))
+		return (p1(s + 1, l - 2));
+	return (0);
+/**
+ * is_palindrome - palindrome
+ * @s: string returned
+ * Return: recursion
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
+}
