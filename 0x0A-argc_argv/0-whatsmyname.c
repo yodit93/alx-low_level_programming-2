3c03a02287b508fe51424470909f1dcf3fbd4874
yodit93 <yoditabebe9391@gmail.com>
2022-07-22 04:53:52 -0700

    print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function to print the file name
+ * @argc: number of argument parameters
+ * @argv: array of list of commands
+ * Return: 0 on success
+ */
+int main(int argc, *argv[])
+{
+	printf("%s\n", *argv);
+}
+
