a7abd7bec57722122cd9acacbf97adab7d1747ff
yodit93 <yoditabebe9391@gmail.com>
2022-07-22 05:51:21 -0700

    print the no of args passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array of arguments listed
  * Return: 0
  */
-int main(int argc, char *argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
