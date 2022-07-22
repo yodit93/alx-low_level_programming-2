b4b77b8a64374bb6dd17b68836f5fe9cc1441cf3
yodit93 <yoditabebe9391@gmail.com>
2022-07-22 09:35:09 -0700

    print the sum of passed arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the multiplication result of two numbers
+ * @argc: arguments pasedd
+ * @argv: array of arguments
+ * Return: 1 or result of multiplication based on condition
+ */
+int main(int argc, char *argv)
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]); /* atoi converts string to number*/
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	
+	printf("%d\n", result);
+	return (0);
+}
+
+
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the sum of passed integers
+ * @argc: arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num, i, j, k;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
